1ff86370d0af61730f0c34ea8e02a9e8f9c57eae
al3j4ndrolm <lopezalejandro5b@gmail.com>
2023-09-30 18:10:57 -0700

    Create a MainScreen.kt class

--- app/src/main/java/com/example/sheepamanager/MainActivity.kt
+++ app/src/main/java/com/example/sheepamanager/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,25 +21,19 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Main Screen")
+                    MainScreen()
+                    GreetingPreview()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     SheepaManagerTheme {
-        Greeting("Android")
+        val mainScreen = MainScreen()
+        mainScreen.Launch()
     }
 }
---
+++ app/src/main/java/com/example/sheepamanager/MainScreen.kt
@@ -0,0 +1,11 @@
+package com.example.sheepamanager
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+class MainScreen {
+    @Composable
+    fun Launch(){
+        Text(text = "Nice day")
+    }
+}
